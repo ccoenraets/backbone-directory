478d56b8f38a71f6d33183e11843c43f77823f60
jspears <speajus@gmail.com>
2012-02-27 20:09:05 -0500

    Update node/employee.js

--- node/employee.js
+++ node/employee.js
@@ -1,5 +1,6 @@
 //We wrap mongoose so that you don't have to include mongoose twice once in the mongoose rest and once here.
 //If I where really smart I would figure out a better way.
+
 module.exports = function(mongoose) {
     var Schema = mongoose.Schema;
     var EmployeeSchema = new Schema({
